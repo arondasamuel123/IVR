38697867f019d3fd9074d70109515f9bed37331d
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-04-22 14:43:10 +0300

    Created Urlpattern for account_balance

--- ivr/urls.py
+++ ivr/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('call/select_service/', ServiceSelection.as_view(), name='language_response'),
     path('call/account_number/', EnterAccountNumber.as_view(), name='enter_account_number'),
     path('call/last_deposit/', ObtainUserDeposit.as_view(), name='last_deposit'),
+    path('call/account_balance/', GetAccountBalance.as_view(), name='account_balance'),
+
 ]
