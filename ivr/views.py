0fa20bc89c41036325115d357766b9dda7bee09e
brayooh <brianndichu.bn@gmail.com>
2020-04-22 18:21:00 +0300

    adds post function to withdraw class

--- ivr/views.py
+++ ivr/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from django.http import HttpResponse
-
+from django.core.exceptions import ObjectDoesNotExist
+from .models import UserBankDetails
 
 # Create your views here.
 class Calls(generics.CreateAPIView):
@@ -39,5 +40,86 @@ class CallResponseForLanguage(generics.CreateAPIView):
                 response['Content-Length'] = len(content)
 
                 return response
+
+        return HttpResponse("Ok")
+
+class EnterAccountNumber(generics.CreateAPIView):
+    def post(self, request, *args, **kwargs):
+        isActive = request.POST.get("isActive")
+        if isActive == '1':
+            digits = request.POST.get("dtmfDigits")
+            if digits == '1':
+                content = """<?xml version="1.0" encoding="utf-8"?><Response><GetDigits timeout="10" finishOnKey="#" 
+                callbackUrl="http://fddb8f91.ngrok.io/call/account_balance/"><Say>Please enter your account number followed by hash to receive account 
+                balance</Say></GetDigits><Say>We did not get any response. Good bye</Say></Response>"""
+                response = HttpResponse(content, content_type="application/xml; charset=utf-8")
+                response['Content-Length'] = len(content)
+
+                return response
+
+            elif digits == '2':
+                content = """<?xml version="1.0" encoding="utf-8"?><Response><GetDigits timeout="10" finishOnKey="#" 
+                callbackUrl="https://c31a6d18.ngrok.io/call/last_deposit/"><Say>Please enter your account number followed by hash to receive your last deposit amount</Say></GetDigits><Say>We did not get any response. Good bye</Say></Response> """
+                response = HttpResponse(content, content_type="application/xml; charset=utf-8")
+                response['Content-Length'] = len(content)
+
+                return response
+
+            elif digits == '3':
+                content = """<?xml version="1.0" encoding="utf-8"?><Response><GetDigits timeout="10" finishOnKey="#" 
+                callbackUrl="https://c31a6d18.ngrok.io/call/last_withdrawal"><Say>Please enter your account number followed by hash to receive your last withdrawal 
+                amount</Say></GetDigits><Say>We did not get any response. Good bye</Say></Response> """
+                response = HttpResponse(content, content_type="application/xml; charset=utf-8")
+                response['Content-Length'] = len(content)
+
+                return response
+
+            elif digits == '4':
+                content = """<?xml version="1.0" encoding="utf-8"?><Response><Say>You will shortly be redirected to 
+                customer care agent</Say></Response> """
+                response = HttpResponse(content, content_type="application/xml; charset=utf-8")
+                response['Content-Length'] = len(content)
+
+                return response
+
+        return HttpResponse("Ok")
+
+
+class GetLastWithdraw(generics.CreateAPIView):
+    def post(self, request, count=[], *args, **kwargs):
+        isActive = request.POST.get("isActive")
+        count.append(1)
+        serializer=None
+        if isActive=='3':
+            digits=request.POST.get("dtmfDigits")
+            withdrawal = UserBankDetails.objects.filter(account_number=digits).first()
+            if withdrawal is not None:
+                fname = str(withdrawal.user_id.first_name)
+                lname = str(withdrawal.user_id.last_name)
+                withdrawal_amount = str(withdrawal.withdrawal)
+                content = """<?xml version="1.0" encoding="utf-8"?><Response><Say> """ + fname + """ """ + lname + """ your last withdraw is """ + withdrawal_amount + """ shillings </Say></Response> """
+                response = HttpResponse(content, content_type="application/xml; charset=utf-8")
+                response['Content-Length'] = len(content)
+
+                return response
+
+            else:
+                length = len(count)
+                if length < 4:
+                    content = """<?xml version="1.0" encoding="utf-8"?><Response><GetDigits timeout="10" 
+                    finishOnKey="#" callbackUrl="https://c31a6d18.ngrok.io/call/last_withdraw/"><Say>The account does not exist. Please enter the correct account number followed by hash</Say></GetDigits><Say>We did not get any response. Good bye</Say></Response> """
+                    response = HttpResponse(content, content_type="application/xml; charset=utf-8")
+                    response['Content-Length'] = len(content)
+
+                    return response
+
+                else:
+                    content = """<?xml version="1.0" encoding="utf-8"?><Response><Say>Number of attempts exceeded. Goodbye</Say></Response>"""
+                    response = HttpResponse(content, content_type="application/xml; charset=utf-8")
+                    response['Content-Length'] = len(content)
+
+                
+
+                    return response
 
         return HttpResponse("Ok")
