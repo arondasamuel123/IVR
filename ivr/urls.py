bbf7042ccbaea78d4266e219dee38d3a7d033056
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-04-23 17:57:35 +0300

    Created a function for Getting account balance

--- ivr/urls.py
+++ ivr/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import Calls, ServiceSelection, EnterAccountNumber, ObtainUserDeposit, GetLastWithdraw
+from .views import Calls, ServiceSelection, EnterAccountNumber, ObtainUserDeposit, GetLastWithdraw,GetAccountBalance
 
 urlpatterns = [
     path('calls/', Calls.as_view(), name='calls'),
     path('call/select_service/', ServiceSelection.as_view(), name='language_response'),
     path('call/account_number/', EnterAccountNumber.as_view(), name='enter_account_number'),
     path('call/last_deposit/', ObtainUserDeposit.as_view(), name='last_deposit'),
-    path('call/last_withdraw/', GetLastWithdraw.as_view(), name='last_withdraw'),    
+    path('call/last_withdraw/', GetLastWithdraw.as_view(), name='last_withdraw'), 
+    path('call/account_balance/',GetAccountBalance.as_view(),name='account_balance'),   
 ]
